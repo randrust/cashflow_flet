4e9d46155bad90a6472246363e96db8b3f8a1c1f
Andrii Rakutskyi <randrur@gmail.com>
2025-01-05 13:00:26 +0200

    Add main window

--- mains.py
+++ mains.py
@@ -1,3 +1,4 @@
+import flet as ft
 import datetime
 from sqlalchemy import Column, Date, Integer, create_engine
 from sqlalchemy.orm import DeclarativeBase, Session
@@ -37,7 +38,7 @@ def add_db_data(data):
 
 def delete_db_data(key):
     with Session(engine) as session:
-        data = session.query(CashFlow).filter(CashFlow.id == key).first()
+        data = session.get(CashFlow, key)
         if data is None:
             return
         session.delete(data)
@@ -51,9 +52,59 @@ def update_db_data(data):
 # data_ins = CashFlow(date=datetime.date(2023, 1, 2), currency1000=2, currency500=2, currency200=2, currency100=2, currency50=2, Summary=3700)
 # add_db_data(data_ins)
 
-data_all = get_db_data()
+# data_all = get_db_data()
 
-for data in data_all:
-    print(data.id, data.date, data.currency1000, data.currency500, data.currency200, data.currency100, data.currency50, data.Summary)
+# for data in data_all:
+#     # print(data.id, data.date, data.currency1000, data.currency500, data.currency200, data.currency100, data.currency50, data.Summary)
+#     print(data.__dict__)
 
-delete_db_data(4)
+# delete_db_data(5)
+        
+def main(page: ft.Page):
+    page.title = "Cash Flow"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    date_from = ft.ElevatedButton(text=datetime.date.today().strftime("%Y-%m-%d"), on_click=lambda e: page.open(
+                ft.DatePicker(first_date=datetime.datetime(year=2025, month=1, day=1),
+                    last_date=datetime.datetime(year=2025, month=12, day=31),)))
+    date_to = ft.ElevatedButton(text=datetime.date.today().strftime("%Y-%m-%d"), on_click=lambda e: page.open(
+                ft.DatePicker(first_date=datetime.datetime(year=2025, month=1, day=1),
+                    last_date=datetime.datetime(year=2025, month=12, day=31),)))
+    
+    
+    
+    data_table = ft.DataTable(
+        columns=[
+            ft.DataColumn(ft.Text("ID")),
+            ft.DataColumn(ft.Text("Date")),
+            ft.DataColumn(ft.Text("Summary")),
+        ],
+        rows=[],
+    )
+
+    data_all = get_db_data()
+    for data in data_all:
+        data_table.rows.append(ft.DataRow(
+            cells=[
+                ft.DataCell(ft.Text(data.id)),
+                ft.DataCell(ft.Text(data.date)),
+                ft.DataCell(ft.Text(data.Summary)),
+            ]))
+
+
+    page.add(
+        ft.Row(
+            [
+                date_from,
+                ft.Text(" <---> "),
+                date_to,
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        ),
+        ft.Row(
+            [data_table,],
+            alignment=ft.MainAxisAlignment.CENTER,
+        ),
+    )
+
+ft.app(main)
